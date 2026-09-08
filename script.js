69807d927a0c700d9dca81565b4253a1400b3ff8
emmanuelowusu17 <oemmanuel17@outlook.com>
2026-09-08 16:07:15 -0600

    Add JavaScript skills list with array and loop

--- script.js
+++ script.js
@@ -9,3 +9,9 @@ document.getElementById("checkButton").addEventListener("click", function() {
     }
 
 });
+let skills = ["HTML", "CSS", "JavaScript", "Git"];
+for (let skill of skills) {
+    let listItem = document.createElement("li");
+    listItem.textContent = skill;
+    document.getElementById("skillsList").appendChild(listItem);
+}
